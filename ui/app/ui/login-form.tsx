2fa8278020e8a0d2b376e4a7038ae469afb1c50d
Johannes Grimm <grimmjohannes1998@gmail.com>
2024-05-17 19:45:16 +0200

    add dummy dashboard

--- ui/app/ui/login-form.tsx
+++ ui/app/ui/login-form.tsx
@@ -15,8 +15,16 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { signIn } from '@/auth';
 import { authenticate } from '@/lib/actions';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from '@/components/ui/card';
+import { Label } from '@radix-ui/react-dropdown-menu';
 
 const formSchema = z.object({
   identifier: z.string().min(2, {
@@ -43,40 +51,66 @@ export function LoginForm() {
     await authenticate(values);
   }
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
-        <FormField
-          control={form.control}
-          name='identifier'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>identifier</FormLabel>
-              <FormControl>
-                <Input placeholder='shadcn' {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name='password'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input type='password' placeholder='********' {...field} />
-              </FormControl>
-              <FormDescription>This is your private password.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type='submit'>Submit</Button>
-      </form>
-    </Form>
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+          <Card className='w-full max-w-sm'>
+            <CardHeader>
+              <CardTitle className='text-2xl'>Login</CardTitle>
+              <CardDescription>
+                Enter your email below to login to your account.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className='grid gap-4'>
+              <div className='grid gap-2'>
+                <FormField
+                  control={form.control}
+                  name='identifier'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>identifier</FormLabel>
+                      <FormControl>
+                        <Input placeholder='shadcn' {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        This is your public display name.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className='grid gap-2'>
+                <FormField
+                  control={form.control}
+                  name='password'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          type='password'
+                          placeholder='********'
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription>
+                        This is your private password.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button type='submit' className='w-full'>
+                Sign in
+              </Button>
+            </CardFooter>
+          </Card>
+        </form>
+      </Form>
+    </>
   );
 }
