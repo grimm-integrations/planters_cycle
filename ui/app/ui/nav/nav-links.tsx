aeac2fca2220698a7c6106d1b5dd9e4e081f69a4
Johannes Grimm <grimmjohannes1998@gmail.com>
2024-05-27 16:48:51 +0200

    chore: Refactor NavLinks component to use TypeScript type for links array

--- ui/app/ui/nav/nav-links.tsx
+++ ui/app/ui/nav/nav-links.tsx
@@ -4,7 +4,14 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-export default function NavLinks({ links }) {
+export default function NavLinks({
+  links,
+}: {
+  links: {
+    name: string;
+    href: string;
+  }[];
+}) {
   const pathname = usePathname();
   return (
     <>
