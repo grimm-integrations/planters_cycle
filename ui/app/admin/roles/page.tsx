f083dae4221e7791983e7c3164a445694577635c
Johannes Grimm <grimmjohannes1998@gmail.com>
2024-05-30 19:08:55 +0200

    refactor roles list to use the list-data component

--- ui/app/admin/roles/page.tsx
+++ ui/app/admin/roles/page.tsx
@@ -1,29 +1,9 @@
-import { MoreHorizontal, PlusCircle, Search } from 'lucide-react';
 import { Metadata } from 'next';
-import Link from 'next/link';
-
-import BreadCrumb from '@/components/bread-crumb';
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table';
 
 import { fetchRoles } from '@/lib/data';
 
-import RoleTableRow from '@/app/ui/admin/roles/table-row';
+import { columns } from '@/app/ui/admin/roles/columns';
+import ListData from '@/app/ui/list-data';
 
 export const metadata: Metadata = {
   title: 'Roles',
@@ -37,65 +17,15 @@ export default async function Page({
     page?: string;
   };
 }) {
-  const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
-
-  const roles = await fetchRoles(query);
+  const roles = await fetchRoles('');
 
   return (
-    <div className='flex min-h-screen w-full flex-col bg-muted/40'>
-      <div className='flex flex-col sm:gap-4 sm:py-4 '>
-        <header className='sticky top-0 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6'>
-          <BreadCrumb />
-          <div className='relative ml-auto flex-1 md:grow-0'>
-            <Search className='absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground' />
-            <Input
-              type='search'
-              placeholder='Search...'
-              className='w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]'
-            />
-          </div>
-          <div className='flex items-center'>
-            <div className='ml-auto flex items-center gap-2'>
-              <Link href='/admin/roles/create'>
-                <Button size='sm' className='h-8 gap-1'>
-                  <PlusCircle className='h-3.5 w-3.5' />
-                  <span className='sr-only sm:not-sr-only sm:whitespace-nowrap'>
-                    Add Roles
-                  </span>
-                </Button>
-              </Link>
-            </div>
-          </div>
-        </header>
-        <main className='grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8'>
-          <Card>
-            <CardHeader>
-              <CardTitle>Roles</CardTitle>
-              <CardDescription>
-                Manage your roles here. You can add, edit, and delete roles.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>Name</TableHead>
-                    <TableHead className='text-right'>
-                      <span className='sr-only'>Actions</span>
-                    </TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {roles?.map((role) => {
-                    return <RoleTableRow key={role.id} role={role} />;
-                  })}
-                </TableBody>
-              </Table>
-            </CardContent>
-          </Card>
-        </main>
-      </div>
-    </div>
+    <ListData
+      name='Role'
+      description='Manage your roles here. You can add, edit, and delete roles.'
+      columns={columns}
+      data={roles}
+      searchParams={searchParams}
+    />
   );
 }
---
+++ ui/app/ui/admin/roles/action-dropdown.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import { Role } from '@prisma/client';
+import { MoreHorizontal } from 'lucide-react';
+import Link from 'next/link';
+import { useState } from 'react';
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { useToast } from '@/components/ui/use-toast';
+
+import { deleteRole, redirectToRoles } from '@/lib/actions';
+
+export default function ActionDropdown({ role }: { role: Role }) {
+  const [open, setOpen] = useState(false);
+
+  const { toast } = useToast();
+
+  async function onClickDelete(role: Role) {
+    try {
+      await deleteRole(role.id);
+      toast({
+        title: 'Success 🎉',
+        description: `Deleted role ${role.name}.`,
+      });
+      await redirectToRoles();
+    } catch (error) {
+      toast({
+        title: 'Uh oh! Something went wrong.',
+        description: `There was a problem with your request.\n${error}`,
+      });
+    }
+  }
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button aria-haspopup='true' size='icon' variant='ghost'>
+            <MoreHorizontal className='h-4 w-4' />
+            <span className='sr-only'>Toggle menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align='end'>
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <Link href={`/admin/roles/${role.id}/edit`}>
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+          </Link>
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <AlertDialog open={open}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete the
+              {role.name} role from the servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={() => setOpen(false)}>
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction onClick={() => onClickDelete(role)}>
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
+}
