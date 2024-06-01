34acafc74e6d1e310a94d0d1a8a76f6108437d49
Johannes Grimm <grimmjohannes1998@gmail.com>
2024-06-01 15:07:09 +0200

    fix: center login form

--- ui/app/login/page.tsx
+++ ui/app/login/page.tsx
@@ -1,13 +1,14 @@
 /*
  * Copyright (c) Johannes Grimm 2024.
  */
-
-import {LoginForm} from '@/app/ui/login-form';
+import { LoginForm } from '@/app/ui/login-form';
 
 export default function LoginPage() {
-    return (
-        <main>
-            <LoginForm/>
-        </main>
-    );
+  return (
+    <div className='w-full min-h-screen flex items-center justify-center '>
+      <div className='mx-auto my-auto '>
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
