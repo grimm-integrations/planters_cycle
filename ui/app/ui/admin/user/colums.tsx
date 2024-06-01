676c5e7a7f9f65f0cd10efaec11194bfe6a99f90
Johannes Grimm <grimmjohannes1998@gmail.com>
2024-06-01 15:08:48 +0200

    feat: add spacing between role badges in user list and add linking

--- ui/app/ui/admin/user/colums.tsx
+++ ui/app/ui/admin/user/colums.tsx
@@ -1,92 +1,98 @@
 /*
  * Copyright (c) Johannes Grimm 2024.
  */
-
 'use client';
 
-import {CompleteUser} from '@/prisma/zod';
-import {ColumnDef} from '@tanstack/react-table';
-import {ArrowUpDown} from 'lucide-react';
+import { CompleteUser } from '@/prisma/zod';
+import { ColumnDef } from '@tanstack/react-table';
+import { ArrowUpDown } from 'lucide-react';
 
-import {Badge} from '@/components/ui/badge';
-import {Button} from '@/components/ui/button';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
 
-import {formatDateToLocal} from '@/lib/utils';
+import { formatDateToLocal } from '@/lib/utils';
 
 import DeleteDropdown from './delete-dropdown';
+import Link from 'next/link';
+
+/*
+ * Copyright (c) Johannes Grimm 2024.
+ */
 
 export const columns: ColumnDef<CompleteUser>[] = [
-    {
-        accessorKey: 'displayName',
-        header: ({column}) => {
-            return (
-                <Button
-                    variant='ghost'
-                    onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-                >
-                    Name
-                    <ArrowUpDown className='ml-2 h-4 w-4'/>
-                </Button>
-            );
-        },
+  {
+    accessorKey: 'displayName',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Name
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      );
     },
-    {
-        accessorKey: 'email',
-        header: ({column}) => {
-            return (
-                <Button
-                    variant='ghost'
-                    onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-                >
-                    Email
-                    <ArrowUpDown className='ml-2 h-4 w-4'/>
-                </Button>
-            );
-        },
+  },
+  {
+    accessorKey: 'email',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Email
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      );
     },
-    {
-        accessorKey: 'lastLogin',
-        header: 'Last Login',
-        meta: {
-            className: 'hidden xl:table-cell',
-        },
-        cell: ({row}) => {
-            return <div>{formatDateToLocal(row.original.lastLogin)}</div>;
-        },
+  },
+  {
+    accessorKey: 'lastLogin',
+    header: 'Last Login',
+    meta: {
+      className: 'hidden xl:table-cell',
     },
-    {
-        accessorKey: 'createdAt',
-        header: 'Created at',
-        meta: {
-            className: 'hidden xl:table-cell',
-        },
-        cell: ({row}) => {
-            return <div>{formatDateToLocal(row.original.createdAt)}</div>;
-        },
+    cell: ({ row }) => {
+      return <div>{formatDateToLocal(row.original.lastLogin)}</div>;
     },
-    {
-        accessorKey: 'roles',
-        header: 'Roles',
-        cell: ({row}) => {
-            const user = row.original;
-            return (
-                <>
-                    {user.roles.map((role) => {
-                        return (
-                            <div key={role.role.id}>
-                                <Badge>{role.role.name}</Badge>
-                            </div>
-                        );
-                    })}
-                </>
-            );
-        },
+  },
+  {
+    accessorKey: 'createdAt',
+    header: 'Created at',
+    meta: {
+      className: 'hidden xl:table-cell',
     },
-    {
-        id: 'actions',
-        cell: ({row}) => {
-            const user = row.original;
-            return <DeleteDropdown user={user}/>;
-        },
+    cell: ({ row }) => {
+      return <div>{formatDateToLocal(row.original.createdAt)}</div>;
     },
+  },
+  {
+    accessorKey: 'roles',
+    header: 'Roles',
+    cell: ({ row }) => {
+      const user = row.original;
+      return (
+        <>
+          <div className='w-full flex'>
+            {user.roles.map((role) => {
+              return (
+                <div className='flex-initial p-0.5' key={role.role.id}>
+                  <Link href={`/admin/roles/${role.roleId}/edit`}><Badge>{role.role.name}</Badge></Link>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      );
+    },
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const user = row.original;
+      return <DeleteDropdown user={user} />;
+    },
+  },
 ];
