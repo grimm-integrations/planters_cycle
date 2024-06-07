f089d37445e00737f53061978a4b71098faa6062
Johannes Grimm <grimmjohannes1998@gmail.com>
2024-06-07 22:04:35 +0200

    refactor: Update columns.tsx to use CompletePlant type and add stage and genetic columns

--- ui/app/ui/plants/columns.tsx
+++ ui/app/ui/plants/columns.tsx
@@ -9,10 +9,10 @@ import { ArrowUpDown } from 'lucide-react';
 
 import ActionDropdown from './action-dropdown';
 
-import type { Plant } from '@prisma/client';
+import type { CompletePlant } from '@/prisma/zod';
 import type { ColumnDef } from '@tanstack/react-table';
 
-export const columns: ColumnDef<Plant>[] = [
+export const columns: ColumnDef<CompletePlant>[] = [
   {
     accessorKey: 'name',
     header: ({ column }) => {
@@ -22,6 +22,37 @@ export const columns: ColumnDef<Plant>[] = [
           variant='ghost'
         >
           Name
+          <ArrowUpDown className='ml-2 size-4' />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: 'stage',
+    header: ({ column }) => {
+      return (
+        <Button
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          variant='ghost'
+        >
+          Stage
+          <ArrowUpDown className='ml-2 size-4' />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: 'genetic',
+    cell: ({ row }) => {
+      return <span>{row.original.genetic.name}</span>;
+    },
+    header: ({ column }) => {
+      return (
+        <Button
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          variant='ghost'
+        >
+          Genetic
           <ArrowUpDown className='ml-2 size-4' />
         </Button>
       );
