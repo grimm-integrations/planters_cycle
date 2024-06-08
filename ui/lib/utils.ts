0267816c10b7fcc4523c7f5bb987da9837694d71
Johannes Grimm <grimmjohannes1998@gmail.com>
2024-06-08 13:43:41 +0200

    refactor: Add formatDateToLocal function for localized date formatting

--- ui/lib/utils.ts
+++ ui/lib/utils.ts
@@ -5,10 +5,22 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+/**
+ * Combines multiple class names into a single string.
+ * @param inputs - The class names to be combined.
+ * @returns The combined class names as a string.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+/**
+ * Formats a given date to a localized string representation.
+ * If the date is not provided or is null, an empty string is returned.
+ * @param date - The date to format.
+ * @param locale - The locale to use for formatting. Defaults to 'de-DE'.
+ * @returns The formatted date string.
+ */
 export const formatDateToLocal = (date?: Date | null, locale = 'de-DE') => {
   if (!date || date == undefined) return '';
   date = new Date(date);
@@ -20,4 +32,3 @@ export const formatDateToLocal = (date?: Date | null, locale = 'de-DE') => {
   const formatter = new Intl.DateTimeFormat(locale, options);
   return formatter.format(date);
 };
-
